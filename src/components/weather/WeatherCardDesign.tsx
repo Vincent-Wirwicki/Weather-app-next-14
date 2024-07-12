b691bcaee75c93f40620b075432091672b9e2a72
Vinz W <vincent.wirwicki@gmail.com>
2024-07-13 00:53:06 +0200

    home page update

--- src/components/weather/WeatherCardDesign.tsx
+++ src/components/weather/WeatherCardDesign.tsx
@@ -5,13 +5,10 @@ export default function WeatherCardDesign() {
   return (
     <>
       <div className="w-[300px] h-full p-5 flex flex-col justify-between ">
-        <Card className="w-[300px] p-5  flex flex-col justify-center items-center">
-          loading
-        </Card>
-        {/* <Card className="p-5">
+        <Card className="p-5">
           <p>A minimalistic weather app</p>
           <p>made with next 14.</p>
-        </Card> */}
+        </Card>
       </div>
     </>
   );
